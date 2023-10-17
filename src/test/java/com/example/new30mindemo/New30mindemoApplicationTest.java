0eeb13f7da67323e5698065fcd58b311305fa8c2
user <mythiliramachandran@gmail.com>
2023-10-17 11:23:38 +0000

    local commit

---
+++ src/test/java/com/example/new30mindemo/New30mindemoApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.new30mindemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class New30mindemoApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
