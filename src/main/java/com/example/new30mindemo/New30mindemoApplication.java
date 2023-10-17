92b359c6dd198cd4a4658093410e42c28bd94471
user <mythiliramachandran@gmail.com>
2023-10-17 09:18:30 +0000

    Committing in Local

---
+++ src/main/java/com/example/new30mindemo/New30mindemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.new30mindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class New30mindemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(New30mindemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/new30mindemo/New30mindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new30mindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class New30mindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
